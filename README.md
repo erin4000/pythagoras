61cd4e618d26a11f0f5dbd264e0a947524987640
erin4000 <erin4000@pm.me>
2022-09-27 00:04:42 +0100

    ​

---
+++ README.md
@@ -0,0 +1,41 @@
+# Efficient ordered generation of primitive Pythagorean triples
+
+Time-efficient generation of primitive pythagorean triples in ascending order.
+
+A **Pythagorean triple** is a triple of positive integers (a,b,c) such that a² + b² = c². A **primitive** Pythagorean triple is a triple such that (a,b,c) have no common factor between them (i.e. are coprime).
+
+The function `pythagorean_triples()` returns an infinite iterator that enumerates all primitive triples (a,b,c) (where a < b) in order of increasing c, followed by increasing a. For example, (20,21,29) will come before (12,35,37) as 29 < 37, while (16, 63, 65) will come before (33, 56, 65) as 16 < 33.
+
+Example usage:
+
+```rs
+// Prints the following:
+// 3² + 4² = 5²
+// 5² + 12² = 13²
+// 8² + 15² = 17²
+// 7² + 24² = 25²
+// 20² + 21² = 29²
+// 12² + 35² = 37²
+// 9² + 40² = 41²
+// 28² + 45² = 53²
+// 11² + 60² = 61²
+// 16² + 63² = 65²
+// 33² + 56² = 65²
+// 48² + 55² = 73²
+// 13² + 84² = 85²
+// 36² + 77² = 85²
+// 39² + 80² = 89²
+// 65² + 72² = 97²
+for (a, b, c) in pythagorean_triples().take_while(|(_,_,c)| *c <= 100) {
+    println!("{}² + {}² = {}²", a, b, c);
+}
+
+// For any primitive triple (a,b,c), a*b*c is a multiple of 60:
+assert!(pythagorean_triples().take(1_000).all(|(a,b,c)| a*b*c % 60 == 0));
+```
+
+## Implementation
+
+Triples are generated efficiently by using a [tree of primitive Pythagorean triples](https://en.wikipedia.org/wiki/Tree_of_primitive_Pythagorean_triples): an infinite ternary tree guaranteed to contain every primitive triple once. Though there is no clear pattern for obtaining the triples in order of increasing hypotenuse, the tree does satisfy the property that the hypotenuse of any triple is strictly less than the hypotenuse of any of its three children.
+
+This suggests a straightfoward algorithm: explore the tree, placing triples in a priority queue, and each time the next triple is pulled out, add the next three elements to the priority queue. This is efficient (O(log n) using a heap data structure) and uses memory linear in the number of triples polled (every poll results in one element being removed from the heap and three being added).
---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
+/// Boilerplate to create a custom sort.
+#[derive(PartialEq, Eq)]
+struct WrappedTriple(i64, i64, i64);
+
+impl Ord for WrappedTriple {
+    /// Ordering is reversed so that the binary heap acts as a min-heap
+    /// rather than a max-heap. If `WrappedTriple` were used anywhere
+    /// else, it would be better to define this the right way around
+    /// and use `std::cmp::Reverse` in the binary heap.
+    fn cmp(&self, other: &Self) -> Ordering {
+        (other.2, other.0).cmp(&(self.2, self.0))
+    }
+}
+
+impl PartialOrd for WrappedTriple {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+const INITIAL_PYTHAGOREAN_TRIPLE: WrappedTriple = WrappedTriple(3, 4, 5);
+const PYTHAGOREAN_MATRICES:
+    [((i64, i64, i64), (i64, i64, i64), (i64, i64, i64)); 3]
+= [
+    (( 1, -2, 2), ( 2, -1, 2), ( 2, -2, 3)),
+    (( 1,  2, 2), ( 2,  1, 2), ( 2,  2, 3)),
+    ((-1,  2, 2), (-2,  1, 2), (-2,  2, 3)),
+];
+
+pub struct PythagoreanTripleIter {
+    min_heap: BinaryHeap<WrappedTriple>,
+}
+
+impl Iterator for PythagoreanTripleIter {
+    type Item = (i64, i64, i64);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Cannot panic as `min_heap` is never empty; it is initialized
+        // with one element, then three more elements are added every
+        // time an elemenet is removed.
+        let WrappedTriple(j, k, l) = self.min_heap.pop().unwrap();
+        for ((a, b, c), (d, e, f), (g, h, i)) in PYTHAGOREAN_MATRICES {
+            let m = a*j + b*k + c*l;
+            let n = d*j + e*k + f*l;
+            let o = g*j + h*k + i*l;
+            self.min_heap.push(WrappedTriple(m.min(n), m.max(n), o));
+        }
+
+        Some((j, k, l))
+    }
+}
+
+/// Produces all primitive Pythagorean triples in sorted order.
+/// That is all coprime triples (a < b < c) such that a^2 + b^2 = c^2,
+/// ordered by increasing c, then a, then b.
+///
+/// Extremely time efficient, but unfortunately uses exponential memory...
+pub fn pythagorean_triples() -> PythagoreanTripleIter {
+    let mut min_heap = BinaryHeap::new();
+    min_heap.push(INITIAL_PYTHAGOREAN_TRIPLE);
+    PythagoreanTripleIter { min_heap }
+}
+
+#[test]
+fn test_ascending() {
+    let mut triple_generator = pythagorean_triples();
+    let mut previous = triple_generator.next().unwrap();
+    for next@(x, _, z) in triple_generator.take(999_999) {
+        let (a, _, c) = previous;
+        assert!((c, a) < (z, x));
+
+        previous = next;
+    }
+}
